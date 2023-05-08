ee1fa89d0acc5484bcfb7da4a1b76c70737c22f7
efeakil <efe.akil35@gmail.com>
2023-05-08 18:29:47 +0300

    multiple student added

--- src/main/java/com/codersnitch/springhibernatedemo/SpringHibernateDemoApplication.java
+++ src/main/java/com/codersnitch/springhibernatedemo/SpringHibernateDemoApplication.java
@@ -16,24 +16,45 @@ public class SpringHibernateDemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+			// createStudent(studentDAO);
+
+			createMultipleStudents(studentDAO);
 		};
+
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		// create student object
+		System.out.println("Creating 3 student object..");
+		Student tempStudent1 = new Student("Efe", "Us", "snitch@gmail.com");
+		Student tempStudent2 = new Student("Alperen", "Kacan", "qincha@gmail.com");
+		Student tempStudent3 = new Student("Yigit", "Sevmeyen", "lui@gmail.com");
+
+		// save the student object
+		System.out.println("Saving 3 student");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent1.getId());
+		System.out.println("Saved student. Generated id: " + tempStudent2.getId());
+		System.out.println("Saved student. Generated id: " + tempStudent3.getId());
+
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
-		//create student object
+		// create student object
 		System.out.println("Creating student object..");
-		Student tempStudent = new Student(
-			"Paul","Walker","paulwalker@gmail.com"
-		);
+		Student tempStudent = new Student("Paul", "Walker", "paulwalker@gmail.com");
 
-		//save the student object
+		// save the student object
 		System.out.println("Saving the student");
 		studentDAO.save(tempStudent);
 
-		//display id of the saved student
+		// display id of the saved student
 		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 
