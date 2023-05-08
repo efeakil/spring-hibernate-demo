347c31b93c469e78e6c1c8bbe6f01cad863612d7
efeakil <efe.akil35@gmail.com>
2023-05-08 17:22:27 +0300

    initial start

---
+++ src/main/java/com/codersnitch/springhibernatedemo/SpringHibernateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codersnitch.springhibernatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codersnitch/springhibernatedemo/SpringHibernateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codersnitch.springhibernatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
